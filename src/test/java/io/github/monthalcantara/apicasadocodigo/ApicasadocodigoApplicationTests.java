521ca11c5bfead68e4c3b9bfad2279596643ea97
monthalcantara <montival_junior@yahoo.com.br>
2020-10-31 14:43:17 -0300

    criacao estrutura projeto

---
+++ src/test/java/io/github/monthalcantara/apicasadocodigo/ApicasadocodigoApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.monthalcantara.apicasadocodigo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApicasadocodigoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
