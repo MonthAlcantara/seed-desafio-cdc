cbdee4ef70c1886ccbaca8dc169d5eaf65f3e098
monthalcantara <montival_junior@yahoo.com.br>
2020-11-11 08:33:12 -0300

    Adicionado repository e comentarios para estudos no controller

--- src/main/java/io/github/monthalcantara/apicasadocodigo/controller/AutorController.java
+++ src/main/java/io/github/monthalcantara/apicasadocodigo/controller/AutorController.java
@@ -3,12 +3,12 @@ package io.github.monthalcantara.apicasadocodigo.controller;
 import io.github.monthalcantara.apicasadocodigo.model.Autor;
 import io.github.monthalcantara.apicasadocodigo.model.dto.request.AutorRequest;
 import io.github.monthalcantara.apicasadocodigo.model.dto.response.AutorResponse;
+import io.github.monthalcantara.apicasadocodigo.validator.ProibeEmailDuplicadoAutorValidator;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.validation.Validator;
+import org.springframework.web.bind.WebDataBinder;
+import org.springframework.web.bind.annotation.*;
 
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
@@ -21,14 +21,26 @@ public class AutorController {
 
     @PersistenceContext
     private EntityManager entityManager;
+    private ProibeEmailDuplicadoAutorValidator proibeEmailDuplicadoAutorValidator;
 
-    public AutorController(EntityManager entityManager){
+    /*
+     * Quando a requisição chega nesse controller, esse método é executado antes de chegar
+     * ao endpoint de destino da requisição para executar as configurações necessarias para execução
+     * desse request relativo a esse controller
+     */
+    @InitBinder
+    public void init(WebDataBinder binder) {
+        binder.addValidators(proibeEmailDuplicadoAutorValidator);
+    }
+
+    public AutorController(EntityManager entityManager, ProibeEmailDuplicadoAutorValidator proibeEmailDuplicadoAutorValidator) {
         this.entityManager = entityManager;
+        this.proibeEmailDuplicadoAutorValidator = proibeEmailDuplicadoAutorValidator;
     }
 
     @PostMapping
     @Transactional
-    public ResponseEntity cadastraNovoAutor(@RequestBody @Valid AutorRequest novoAutorRequest){
+    public ResponseEntity cadastraNovoAutor(@RequestBody @Valid AutorRequest novoAutorRequest) {
         Autor novoAutor = novoAutorRequest.converteParaAutor();
         entityManager.persist(novoAutor);
         return new ResponseEntity(new AutorResponse(novoAutor), HttpStatus.OK);
---
+++ src/main/java/io/github/monthalcantara/apicasadocodigo/repository/AutorRepository.java
@@ -0,0 +1,11 @@
+package io.github.monthalcantara.apicasadocodigo.repository;
+
+import io.github.monthalcantara.apicasadocodigo.model.Autor;
+import org.springframework.data.repository.CrudRepository;
+
+import java.util.Optional;
+
+public interface AutorRepository extends CrudRepository<Autor, Integer> {
+
+    Optional<Autor> findByEmail(String email);
+}
